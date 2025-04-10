6975d31f33f6e340c783f69fd74e66ab6118c73a
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2025-04-10 18:29:26 +0530

    minor updates

--- src/main/java/com/testing/todoapp/config/WebMVCConfig.java
+++ src/main/java/com/testing/todoapp/config/WebMVCConfig.java
@@ -1,15 +1,19 @@
 package com.testing.todoapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
+    @Value("${spring.cors.url")
+    private String url;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(url)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
